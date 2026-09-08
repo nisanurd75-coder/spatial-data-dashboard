6b4fb25b002291aee5e66096421efc38a447011b
nisanurd75-coder <nisanurd75@gmail.com>
2026-09-08 22:37:29 +0300

    İlk kurulum: Temel dosya yapısı oluşturuldu

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Mekânsal Dashboard", layout="wide")
+
+st.title("🗺️ GeoJSON / KML Veri Analiz Paneli")
+st.write("Harita mühendisliği projemizin ilk adımı!")
